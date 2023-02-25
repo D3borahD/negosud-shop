59e82a93320ea5bb9d4454939fdf0c709562c4c5
Deborah <deborah.douillet@aum.bio>
2023-02-25 08:20:44 +0100

    set up favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
+/* ----- modules ----- */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 
+/* ----- components ----- */
 import { AppComponent } from './app.component';
-import {RouterModule} from "@angular/router";
-import {NavbarComponent} from "./components/navbar/navbar.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'navbar', component: NavbarComponent}
+      {path: 'navbar', component: NavbarComponent},
+      {path: 'footer', component: FooterComponent},
     ])
   ],
   providers: [],
