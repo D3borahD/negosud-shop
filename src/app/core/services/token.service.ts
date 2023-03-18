a64e4b14d1a4ba2e8f42938448f1448deb38a6d9
Deborah <deborah.douillet@aum.bio>
2023-03-18 18:09:53 +0100

    STABLE STATE

--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -26,12 +26,36 @@ export class TokenService {
     this.router.navigate(['/'])
   }
 
-  getToken(): string | null {
-    return localStorage.getItem('token')
-  }
-
   clearTokenExpired() {
     localStorage.removeItem('token')
     this.router.navigate(['connexion'])
   }
+
+  getToken(): string | null {
+    return localStorage.getItem('token')
+  }
+
+
+
+/*  getPayload(){
+    let user: ITokenUser = {
+      id: 0,
+      nom: '',
+      prenom: '',
+      email: ''
+    }
+
+    let token = localStorage.getItem('token')
+    if(token != null){
+      const decode: ITokenUser =  jwtDecode<ITokenUser>(token)
+      user.id = decode.id
+      user.nom = decode.nom
+      user.prenom = decode.prenom
+      user.email = decode.email
+    }
+
+    return user
+
+  }*/
+
 }
